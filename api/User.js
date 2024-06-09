ca39848adb9a6c4de220ca12bdbef7666d833922
@networkpankaj <@networkpankaj>
2024-06-09 15:33:46 +0000

    Initialize

---
+++ api/User.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// SignUp
+
+router.post('/signup', (req, res) => {
+
+}
+)
+
+router.post('/signin', (req, res) => {
+
+})
+
+module.exports = router;
